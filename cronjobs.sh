6506191bea12d1cf661bd7f541611376c120461f
emmanuel <emmanuel.ekwebelem@stonybrook.edu>
2022-10-27 14:35:30 +0000

    Update

--- cronjobs.sh
+++ cronjobs.sh
@@ -1,14 +1,11 @@
 # Schedule of Cron Jobs
 
-Once A Day at 12:00AM
-
+# Once A Day at 12:00AM
 0 0 * * * /home/useradmin/crontab/crontab.py
 
-Every Sunday night at 10:00PM
-
+# Every Sunday night at 10:00PM
 0 22 * * SUN /home/useradmin/crontab/crontab.py
 
 
-Every 3 Months at 8:00AM
-
+# Every 3 Months at 8:00AM
 0 08 1 */3 * /home/useradmin/crontab/crontab.py > log.txt 2>&1 &
